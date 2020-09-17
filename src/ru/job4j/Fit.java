e4f3d617a28fcc75ef8c8e46286166ca0658d0e7
Давлат <davlat51@gmail.com>
2020-09-18 00:44:16 +0300

    Идеальный вес

--- src/ru/job4j/Fit.java
+++ src/ru/job4j/Fit.java
@@ -4,12 +4,13 @@ public class Fit {
     public static class Fit2 {
 
         public static double manWeight(short height) {
-            double rsl = ((height – 100) * 1,15);
+            double rsl = (height-100) * 1.15;
+
             return rsl;
         }
 
         public static double womanWeight(short height) {
-            double rsl = (height – 110) * 1,15.;
+            double rsl = (height-110) * 1.15;
             return rsl;
         }
 
