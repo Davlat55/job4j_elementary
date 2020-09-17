f036b04c33c05ec84a529a3c778f2cf635e41566
Давлат <davlat51@gmail.com>
2020-09-18 00:38:13 +0300

    Идеальный вес

--- src/ru/job4j/Fit.java
+++ src/ru/job4j/Fit.java
@@ -15,9 +15,9 @@ public class Fit {
 
         public static void main(String[] args) {
             short height = 187;
-            double man = Fit.manWeight(height);
+            double man = Fit2.manWeight(height);
             System.out.println("Man 187 is " + man);
-            double woman = Fit.womanWeight(height);
+            double woman = Fit2.womanWeight(height);
             System.out.println("Woman 187 is " + woman);
         }
 
