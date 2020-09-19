071c603aad0c78225ea83dc76164a96f1f2c8f13
Давлат <davlat51@gmail.com>
2020-09-19 17:48:44 +0300

    Расстояние между точками - Тест

--- src/ru/job4j/condition/PointTest.java
+++ src/ru/job4j/condition/PointTest.java
@@ -10,10 +10,10 @@ public class PointTest {
         double result = PointTest.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
 
-        double result = PointTest.distance(0, 2, 2, 2);
-        System.out.println("result (0, 2) to (2, 2) " + result);
+        double result2 = PointTest.distance(0, 2, 2, 2);
+        System.out.println("result (0, 2) to (2, 2) " + result2);
 
-        double result = PointTest.distance(2, 2, 3, 3);
-        System.out.println("result (2, 2) to (3, 3) " + result);
+        double result3 = PointTest.distance(2, 2, 3, 3);
+        System.out.println("result (2, 2) to (3, 3) " + result3);
     }
 }
