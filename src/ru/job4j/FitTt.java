a48fc799d7bd7857e694775843dcff265928e722
Давлат <davlat51@gmail.com>
2020-09-19 17:19:44 +0300

    Идеальный вес - Тест

---
+++ src/ru/job4j/FitTt.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class FitTt {
+    public static double manWeight(short height) {
+        double rsl = -1;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = -1;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = FitTt.manWeight(height);
+        System.out.println("Man 187 is " + man);
+    }
+}
