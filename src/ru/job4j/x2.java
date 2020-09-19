1d362dfe5d5df5006afc51ef9b714e775b4ed6f4
Давлат <davlat51@gmail.com>
2020-09-19 16:53:37 +0300

    квадратное уравнение

--- src/ru/job4j/x2.java
+++ src/ru/job4j/x2.java
@@ -12,11 +12,11 @@ public class x2 {
         int x = 1;
         int rsl = x2.calc(a, b, c, x);
         System.out.println(rsl);
-        int a = 1;
-        int b = 1;
-        int c = 1;
-        int x = 1;
-        int rsl = x2.calc(a, b, c, x);
+        int a1 = 1;
+        int b1 = 1;
+        int c1 = 1;
+        int x1 = 1;
+        int rsl1 = x2.calc(a1, b1, c1, x1);
         System.out.println(rsl);
     }
 }
