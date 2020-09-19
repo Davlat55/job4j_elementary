173cb83ca5a6cd007b0cb722571a428a0ef4a87f
Давлат <davlat51@gmail.com>
2020-09-19 10:02:56 +0300

    Конвертор (тест)

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -3,9 +3,8 @@ package ru.job4j.condition;
 public class Greeting {
    public static void main(String[] args) {
       String idea = "I like Java ";
-
       System.out.println(idea);
-      idea + =" But I am a newbie.";
+      idea += " But I am a newbie.";
       System.out.println(idea);
       int year = 2020;
       System.out.println(idea + year);
--- src/ru/job4j/converter/ConverterTest.java
+++ src/ru/job4j/converter/ConverterTest.java
@@ -12,13 +12,14 @@ public class ConverterTest {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
+        float euro = rubleToEuro(140);
         float dollor = rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollor + " dollor.");
         int in = 140;
         int expected = 2;
-        float out = Converter.rubleToEuro(in);
+        float out;
+        out = rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
     }
