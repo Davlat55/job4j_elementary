08b8701bd08afdd46583545cfe6e28ac6097e48f
Давлат <davlat51@gmail.com>
2020-09-19 18:26:09 +0300

    Операции сравнения [#362629]

--- src/ru/job4j/condition/SqAreaT.java
+++ src/ru/job4j/condition/SqAreaT.java
@@ -5,7 +5,9 @@ public class SqAreaT {
         double rsl = (p * k) / (2 * (k + 1));
         return rsl;
     }
+
     public static void main(String[] args) {
         double result1 = SqAreaT.square(4, 1);
         System.out.println(" p = 4, k = 1, real = " + result1);
+    }
 }
