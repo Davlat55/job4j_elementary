99d090fd177cad1366c9535c7c99b2b52b0d6138
Давлат <davlat51@gmail.com>
2020-09-19 12:14:28 +0300

    Конвертор тест

--- test/ru/job4j/converter/ConverterTestTest.java
+++ test/ru/job4j/converter/ConverterTestTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.Assert;
 import ru.job4j.Convertor;
 
-public class ConverterTestTest extends TestCase {
+class Converter {
 
     public void testRubleToEuro() {
     }
