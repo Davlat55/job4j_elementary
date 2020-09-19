2ef716621dd9763abc271c5890bce92eaaf9d5f1
Давлат <davlat51@gmail.com>
2020-09-19 17:31:50 +0300

    Идеальный вес - Тест

--- src/ru/job4j/FitTt.java
+++ src/ru/job4j/FitTt.java
@@ -14,6 +14,6 @@ public class FitTt {
     public static void main(String[] args) {
         short height = 187;
         double man = FitTt.manWeight(height);
-        System.out.println("Мужчина при росте 187 вес должен быть " + man);
+        System.out.println("Мужчина при росте 187 должен иметь вес " + man);
     }
 }
