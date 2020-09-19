604fe75e255661e031bafc2c3897c343808fd8a9
Давлат <davlat51@gmail.com>
2020-09-19 18:35:37 +0300

    Деление на ноль - #362642

--- src/ru/job4j/AlertDivByZero.java
+++ src/ru/job4j/AlertDivByZero.java
@@ -7,9 +7,8 @@ public class AlertDivByZero {
     }
 
     public static void possibleDiv(int number) {
-        boolean result = number == 0;
-        if (result) {
-            System.out.println("Could not div by 0.");
+        if (number > 0) {
+            System.out.println("This is negative numbers");
         }
     }
 }
