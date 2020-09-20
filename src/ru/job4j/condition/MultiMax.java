4aa1cd238e5fbb9db58169ba666b32389af5dd9b
Давлат <davlat51@gmail.com>
2020-09-20 14:29:06 +0300

    Максимум из трех

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -4,20 +4,17 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        if (first > second) {
-            if (first > third) {
-                return first;
+        if (first < second) && (first > third){
+             return first;
             }
-            if (second > third) {
-                return second;
-            }
-            return third;
-        }
+            return (second > third) ? second : third;
+     }
+
+
+    public static void main(String[] args) {
+        int rsl = MultiMax.max(2, 4, 6);
+        System.out.println(rsl);
     }
 }
-    public static void main(String[] args) {
-        int rsl = MultiMax.max(2,4,6);
-        System.out.println(rsl);
-
 
 
