8035af6ac27b7ac17260d20d71f80063ffb7a90d
Давлат <davlat51@gmail.com>
2020-09-20 10:45:27 +0300

    Cуществует ли треугольник - #362643

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        int result = ;
+        int result = true;
         return result;
     }
 }
