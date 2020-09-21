8a9875634bacf5be9e71d4aca15408c5d1386bcd
Давлат <davlat51@gmail.com>
2020-09-21 07:48:48 +0300

    максимум из трех - тест - 362627

--- src/ru/job4j/condition/ThreeMax.java
+++ src/ru/job4j/condition/ThreeMax.java
@@ -2,13 +2,13 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-       if ((first > second) && (first > third)) {
+       if ((first >= second) && (first >= third)) {
             return first;
         }
-        if (second > first && second > third) {
+        if (second >= first && second >= third) {
             return  second;
         }
-        return first;
+        return third;
     }
 
     public static void main(String[] args) {
