0becc9efb63a8134d72b6896e7cfaba006fc4819
Давлат <davlat51@gmail.com>
2020-09-24 00:12:33 +0300

    Проверка на простое число (в работе)

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,13 +4,13 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = false;
         for (int i = 2; i <= number; i++) {
-            if (number % i == 0 ) {
-                 prime = true;
+            if (number % i == 0 && number == i) {
+                  prime = false;
                     break;
-                }
-                return false;
+                } else
+                prime = true ;
             }
-      return prime;
+        return false;
     }
 
 }
