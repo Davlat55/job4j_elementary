e0a2996653b5a47b0a773988991b863ff6c15a76
Давлат <davlat51@gmail.com>
2020-09-25 01:01:17 +0300

    Объявление массива, размер его и заполнение

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -14,9 +14,11 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
-        for (int i = 0; i < 4; i++) {
-            System.out.println(names[i]);
-        }
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 
 }
