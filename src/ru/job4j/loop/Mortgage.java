ff80cccc70cb4b5a4920d9ffcd4b4b1f5486c3a7
Давлат <davlat51@gmail.com>
2020-09-24 22:58:20 +0300

    Ипотека [#362738]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -1,14 +1,16 @@
 package ru.job4j.loop;
 
 public class Mortgage {
-    public static int year(int amount, int salary, double percent) {
+    public static int year(double amount, int salary, double percent) {
         int year = 0;
         double p = 1;  // это ежегодный платеж
         while (p > 0) {
             p = (((amount * percent) / 100) + amount) - salary;
+            amount = p;  // Здесь разные тип данных получилось, и из за этого тип amount сменил на double
             year++;
         }
         return year;
 
     }
+
 }
