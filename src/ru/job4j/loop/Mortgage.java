f2e9aec558c4086bc226cc8fc18e674ea2906711
Давлат <davlat51@gmail.com>
2020-09-25 22:05:30 +0300

    ипотека (в работе)

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -1,16 +1,16 @@
 package ru.job4j.loop;
 
 public class Mortgage {
-    public static int year(double amount, int salary, double percent) {
+    public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double p = 1;  // это ежегодный платеж
-        while (p > 0) {
-            p = (((amount * percent) / 100) + amount) - salary;
-            amount = p;  // Здесь разные тип данных получилось, и из за этого тип amount сменил на double
-            year++;
+        while (((amount * (percent/10)) + amount) > 0) {
+            ((amount * (percent/10)) + amount) - salary;
+               year++;
         }
         return year;
+   }
 
+    public static void main(String[] args) {
+        System.out.println(Mortgage.year(100,70,50));
     }
-
 }
