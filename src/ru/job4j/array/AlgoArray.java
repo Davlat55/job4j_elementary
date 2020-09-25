8af5287be80654efc7bde2fb838469c26eb860a4
Давлат <davlat51@gmail.com>
2020-09-25 23:11:26 +0300

    Меняем местами элементы

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+
+
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0]; /* переменная для временного хранения значение ячейки с индексом 0. */
+        array[0] = array[3]; /* записываем в ячейку с индексом 0 значение ячейки с индексом 3. */
+        array[3] = temp; /* записываем в ячейку с индексом 3 значение временной переменной. */
+
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+        System.out.println(" ");
+        // меняем местами 3 и 2
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        // меняем местами 5 и 4
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
+
+       for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
