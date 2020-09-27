b01ba32a7849e08f2ba653df4f3b9e2af3096369
Давлат <davlat51@gmail.com>
2020-09-27 11:13:33 +0300

    Поиск индексе в диапазоне

--- src/ru/job4j/array/FindLoop2.java
+++ src/ru/job4j/array/FindLoop2.java
@@ -4,24 +4,24 @@ public class FindLoop2 {
 
         public static int indexOf(int[] data, int el) {
             int rst = -1;
-            for (int i = 0; i < data.length; i++) {
+            for (int i = 0; i <= data.length; i++) {
                 if(data[i] == el) {
                     rst = i;
                     break;
                 }
-                rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+
               }
             return rst;
         }
 
         public static int indexOf(int[] data, int el, int start, int finish) {
             int rst = -1;
-            for ( int i = start; i < finish; i++) {
+            for ( int i = start; i <= finish; i++) {
                 if(data[i] == el){
                     rst = i;
                     break;
                 }
-                rst = -1;
+
             }
             return rst;
         }
