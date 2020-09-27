7a83b7a1a2c443c6354f4016f66463178c5cecd4
Давлат <davlat51@gmail.com>
2020-09-27 11:24:04 +0300

    Слово начинается с ... [#362649]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,12 +4,10 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            if(!(word[i] == pref[i])) {
+            if (word[i] != pref[i]) {
                 result = false;
                 break;
-            } else
-                result = true;
-
+            }
         }
         return result;
     }
