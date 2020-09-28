870792c26fb8d1473ca7450920f00ac231d357dc
Давлат <davlat51@gmail.com>
2020-09-28 23:37:17 +0300

    Кофе машина

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -7,16 +7,18 @@ public class Machine {
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
-        for ( size = 0; size < COINS.length; size++) {
-            if((money - price) % COINS[size] != 0) {
-                rsl[size] = COINS[size];
-            } else {
-                rsl[size] = COINS[size];
-                break;
+            for (size = 0; size < COINS.length; size++) {
+                if ((money - price) % COINS[size] != 0) {
+                    rsl[size] = COINS[size];
+                } else {
+                    rsl[size] = COINS[size];
+                    break;
+                }
             }
-        }
-        return Arrays.copyOf(rsl, size+1);
-    }
-
-
+        return Arrays.copyOf(rsl, size + 1);
+   }
 }
+
+
+
+
