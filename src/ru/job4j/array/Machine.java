793cb84ce788284d9e4e02252b7afa7bddc91daa
Давлат <davlat51@gmail.com>
2020-09-29 22:06:13 +0300

    Кофе машина

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -3,16 +3,23 @@ import java.util.Arrays;
 
 public class Machine {
     private final int[] COINS = {10, 5, 2, 1};
-
+    int size = 0;
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
-        int size = 0;
-            for (int i = size; i < COINS.length; i++) {
-                while ((money - price) > 0) {
+        if ((money -= price) > 0) {
+            for (size = 0; size < COINS.length; size++) {
+                while (money % COINS[size] >= 0) {
                     rsl[size] = COINS[size];
                     money -= COINS[size];
+                    if (money < COINS[size]) {
+                         break;
+                     }
+                }
+                if (money == 0) {
+                    break;
                 }
             }
+        }
         return Arrays.copyOf(rsl, size );
    }
 }
