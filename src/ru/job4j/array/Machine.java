66df3fc63a98c8e76dd158b9f6c2cf6f71b7eb42
Давлат <davlat51@gmail.com>
2020-09-29 22:16:57 +0300

    Кофе машина

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -6,13 +6,13 @@ public class Machine {
     int size = 0;
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
-        if ((money -= price) > 0) {
-            for (size = 0; size < COINS.length; size++) {
-                while (money % COINS[size] >= 0) {
-                    rsl[size] = COINS[size];
-                    money -= COINS[size];
-                    if (money < COINS[size]) {
-                         break;
+        if ((money -= price) > 0) {                          // отдаем кофе и считаем остаток
+            for (size = 0; size < COINS.length; size++) {    // начинаем делит сдачу на монеты
+                while (money % COINS[size] >= 0) {           // сдачу на монету
+                    rsl[size] = COINS[size];                 // помещаем первую монету
+                    money -= COINS[size];                    // вычитаем монету
+                    if (money < COINS[size]) {               // если остаток меньше монеты,
+                         break;                              // прериваем цикл, и переходим к следующей монете
                      }
                 }
                 if (money == 0) {
@@ -20,7 +20,7 @@ public class Machine {
                 }
             }
         }
-        return Arrays.copyOf(rsl, size );
+        return Arrays.copyOf(rsl, size+1 );
    }
 }
 
