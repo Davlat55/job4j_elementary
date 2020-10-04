cd0e902ed1bb934f3d4090fe19028f123d84a316
Давлат <davlat51@gmail.com>
2020-10-04 13:55:34 +0300

    Соединить два массива, без сортировки по возрастании

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,14 +3,22 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
+        int indexL = 0, indexR = 0;
+        for (int i = 0; i < left.length + right.length; i++) {
+            if(indexL == left.length) {
+                rsl[i] = right[i - indexR];
+                indexR++;
+            } else if(indexR == right.length){
+                rsl[i] = left[i - indexL];
+                indexL++;
+            } else if(left[i - indexL] < right[i - indexR]) {
+                rsl[i] = left[i - indexL];
+                indexR++;
+            } else {
+                rsl[i] = right[i = indexR];
+                indexL++;
+            }
         }
-        for (int y = left.length; y < left.length + right.length; y++) {
-            rsl[y] =  right[y - left.length];
-        }
-        SortSelected.sort(rsl);
-
         return rsl;
     }
 }
