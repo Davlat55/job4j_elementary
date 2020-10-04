5f195748462e9515a2821b30ed39e961acff78e5
Давлат <davlat51@gmail.com>
2020-10-04 08:19:52 +0300

    Соединить два массива

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i, i < left.length){
+        for (int i = 0; i < left.length; i++) {
             rsl[i] = left[i];
         }
-        int i = left.length + 1;
-        for (i, i < left.length + right.length, i++) {
+        int i = left.length;
+        for (; i < left.length + right.length; i++) {
             rsl[i] =  right[i - left.length];
         }
         return rsl;
