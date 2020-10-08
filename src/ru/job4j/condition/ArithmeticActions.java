3cb7153a110cc72c137e636f0ac17033ca7bbf2e
Давлат <davlat51@gmail.com>
2020-10-08 04:26:44 +0300

    Арифметические операции

---
+++ src/ru/job4j/condition/ArithmeticActions.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class ArithmeticActions {
+    public static String selectAction(int left, int right, int rsl) {
+        if (rsl == left + right)
+            return "added";
+        if (rsl == left - right)
+            return "subtracted";
+        if (rsl == left * right)
+            return "multiplied";
+        if (rsl == left / right){
+            return "divided";
+        } else
+            return "none";
+    }
+}
